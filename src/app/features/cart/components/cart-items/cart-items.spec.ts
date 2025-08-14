fe35c4c5b221cb9525d7c32005486e93022f6796
omnyaahmed959 <omnyaahmed959@gmail.com>
2025-08-14 05:09:37 +0300

    Cart-Items

--- src/app/features/cart/components/cart-items/cart-items.spec.ts
+++ src/app/features/cart/components/cart-items/cart-items.spec.ts
@@ -1,16 +1,16 @@
-/* import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { CartItems} from './features/cart/components/cart-items'; // غيرت الـ import
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {CartItemsComponent} from  './cart-items'; 
 
-describe('CartItems', () => {
-  let component: CartItems; // غيرت من CartItems
-  let fixture: ComponentFixture<CartItems>;
+describe('CartItemsComponent', () => {
+  let component: CartItemsComponent; // غيرت من CartItems
+  let fixture: ComponentFixture<CartItemsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CartItems] // غيرت من CartItems
+      imports: [CartItemsComponent] // غيرت من CartItems
     }).compileComponents();
 
-    fixture = TestBed.createComponent(CartItems);
+    fixture = TestBed.createComponent(CartItemsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -24,4 +24,4 @@ describe('CartItems', () => {
     component.startShopping();
     expect(console.log).toHaveBeenCalledWith('Redirecting to shop...');
   });
-}); */
+});
