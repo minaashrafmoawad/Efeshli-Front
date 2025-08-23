6c2899e01fc7fa2b74c779684311a2b8a00e6498
mina_ <mina_@DESKTOP-1V9UF48>
2025-08-23 04:32:09 +0200

    integrate home - login - register - auth service - gards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,41 +1,94 @@
 import { provideRouter, Routes } from '@angular/router';
-import { LoginComponent } from './features/auth/components/login/login';
-import { PaymentMethods } from './features/checkout/components/payment-methods/payment-methods';
-import { EmailConfirmationComponent } from './features/auth/components/EmailConfirmation/EmailConfirmation.component';
-
-import { ProductDetailPageComponent } from './features/products/components/product-details/product-details';
-import { SignupComponent } from './features/auth/components/signup/signup';
-import { AuthGuard } from './core/guards/auth-guard';
 import { ReverseAuthGuard } from './core/guards/reverse-auth-guard';
 import { CartItemsComponent } from './features/cart/components/cart-items/cart-items';
 import { FullCartItemsComponent } from './shared/components/full-cart/full-cart-items';
+import { HomeComponent } from './features/home/home.component';
+import { EmailConfirmationComponent } from './features/auth/components/EmailConfirmation/EmailConfirmation.component';
+import { ProductDetailPageComponent } from './features/products/components/product-details/product-details';
+import { AuthGuard } from './core/guards/auth-guard';
 
-export const routes: Routes =[
-// login 
+export const routes: Routes = [
   { path: 'login',  loadComponent: () => import('./features/auth/components/login/login').then(m => m.LoginComponent),canActivate:[AuthGuard] },
-  // register
-  { path: 'signup',  loadComponent: () => import('./features/auth/components/signup/signup').then(m => m.SignupComponent),
-    canActivate: [AuthGuard] },
-  {path: 'confirm-email', component: EmailConfirmationComponent, canActivate: [AuthGuard,ReverseAuthGuard]},
+  {path:'signup',loadComponent:()=>import('./features/auth/components/signup/signup').then(m=>m.SignupComponent)},
+  { path: 'confirm-email', component: EmailConfirmationComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: HomeComponent },
   { path: 'cart', component: CartItemsComponent },
   { path: 'full-cart', component: FullCartItemsComponent },
-   { path: 'product-details/:id', component: ProductDetailPageComponent },
-
-  // { path: 'product-details/:id', component: ProductDetailPageComponent },
-// payment
-// {
-//     path: 'payment',
-//     loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent),
-//     canActivate: [ReverseAuthGuard] // Require authentication
-//   },
-  // { path: 'products/:slug', component: ProductDetailPageComponent },
-    // { path: '', redirectTo: 'login', pathMatch: 'full' },
-
-  // { path: '**', redirectTo: '' }
-
+  { path: 'product-details/:id', component: ProductDetailPageComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' }
 ];
 
 export const appRouterProviders = provideRouter(routes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { provideRouter, Routes } from '@angular/router';
+// import { LoginComponent } from './features/auth/components/login/login';
+// import { PaymentMethods } from './features/checkout/components/payment-methods/payment-methods';
+// import { EmailConfirmationComponent } from './features/auth/components/EmailConfirmation/EmailConfirmation.component';
+
+// import { ProductDetailPageComponent } from './features/products/components/product-details/product-details';
+// import { SignupComponent } from './features/auth/components/signup/signup';
+// import { AuthGuard } from './core/guards/auth-guard';
+// import { ReverseAuthGuard } from './core/guards/reverse-auth-guard';
+// import { CartItemsComponent } from './features/cart/components/cart-items/cart-items';
+// import { FullCartItemsComponent } from './shared/components/full-cart/full-cart-items';
+// import { HomeComponent } from './features/home/home.component';
+
+// export const routes: Routes =[
+// // login 
+//   { path: 'login',  loadComponent: () => import('./features/auth/components/login/login').then(m => m.LoginComponent),canActivate:[AuthGuard] },
+//   // register
+//   { path: 'signup',  loadComponent: () => import('./features/auth/components/signup/signup').then(m => m.SignupComponent),
+//     canActivate: [AuthGuard] },
+//   {path: 'confirm-email', component: EmailConfirmationComponent, canActivate: [AuthGuard,ReverseAuthGuard]},
+//   {path:'home',component:HomeComponent},
+  
+//   { path: 'cart', component: CartItemsComponent },
+//   { path: 'full-cart', component: FullCartItemsComponent },
+//    { path: 'product-details/:id', component: ProductDetailPageComponent },
+
+//   // { path: 'product-details/:id', component: ProductDetailPageComponent },
+// // payment
+// // {
+// //     path: 'payment',
+// //     loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent),
+// //     canActivate: [ReverseAuthGuard] // Require authentication
+// //   },
+//   // { path: 'products/:slug', component: ProductDetailPageComponent },
+//     { path: '', redirectTo: 'home' },
+
+//   // { path: '**', redirectTo: '' }
+
+// ];
+
+// export const appRouterProviders = provideRouter(routes);
 
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5104/api'
+  // apiUrl: 'http://localhost:5104/api'
+  apiUrl: 'http://efreshliapi.runasp.net/api'
 };
