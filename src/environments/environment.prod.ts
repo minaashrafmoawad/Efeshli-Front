de62612eb5a6ef62ecd4bb469bcc5adecb10739c
mina_ <mina_@DESKTOP-1V9UF48>
2025-08-24 22:44:12 +0200

    jj

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
-
 export const environment = {
   production: true,
-  apiUrl: (window as any)["env"]["apiUrl"] || "http://localhost:5104/api"
+  apiUrl: (window as any)["env"]["API_URL"] || "http://localhost:5104/api"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5104/api'
-  // apiUrl: 'http://efreshliapi.runasp.net/api'
+  apiUrl: "http://localhost:5104/api"
 };
