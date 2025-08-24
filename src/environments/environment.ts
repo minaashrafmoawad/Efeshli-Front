c77cb334c72d7e570783911055060ae03cf178af
Mina Ashraf <76844952+minaashrafmoawad@users.noreply.github.com>
2025-08-24 22:50:37 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: (window as any)["env"]["API_URL"] || "http://localhost:5104/api"
+  production: false,
+  apiUrl: 'http://localhost:5104/api'
 };
