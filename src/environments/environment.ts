b814509a729ae92e0b771b22478cec6c9def2c47
mina_ <mina_@DESKTOP-1V9UF48>
2025-08-24 23:20:19 +0200

    j

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: "http://localhost:5104/api"
+  production: true,
+  apiUrl: (window as any)["env"]["API_URL"] || "http://localhost:5104/api"
 };
